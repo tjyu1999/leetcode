6541869bd897f8145645c9b543e7915e44f16471
tjyu1999 <tjyu1999@gmail.com>
2024-10-08 20:32:15 +0800

    add 0141.cpp

---
+++ 0141.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode *fast = head;
+        ListNode *slow = head;
+        
+        while(fast != nullptr && fast->next != nullptr) {
+            fast = fast->next->next;
+            slow = slow->next;
+            
+            if(fast == slow)
+                return true;
+        }
+        
+        return false;
+    }
+};
+
+// fast-slow pointer
