b542a136628ac71dba7522d95f3ad44456f94649
tjyu1999 <tjyu1999@gmail.com>
2024-10-08 21:02:04 +0800

    edit 0070.cpp

--- 0070.cpp
+++ 0070.cpp
@@ -1,20 +1,18 @@
-/*
- *  time complexity: O(n)
- *  space complexity: O(1)
- */
+class Solution {
+public:
+    int climbStairs(int n) {
+        int prev = 1;
+        int curr = 1;
 
-class Solution{
-    public:
-        int climbStairs(int n){
-            int prev1 = 1;
-            int prev2 = 1;
-            
-            for(int i = 2; i <= n; i++){
-                int curr = prev1 + prev2;
-                prev2 = prev1;
-                prev1 = curr;
-            }
-            
-            return prev1;
+        for (int i = 2; i <= n; ++i) {
+            const int temp = prev;
+            prev = curr;
+            curr += temp; 
+        }
+
+        return curr;
     }
 };
+
+// dynamic programming
+// starting from 2nd stair is faster
