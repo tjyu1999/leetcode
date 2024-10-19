1f65c67d9d5e313543d2c2abf073887bdeea65a0
tjyu1999 <tjyu1999@gmail.com>
2024-10-19 09:22:16 +0800

    add 0035.cpp

---
+++ 0035.cpp
@@ -0,0 +1,27 @@
+/* search insert position
+ *
+ * input: nums = [1,3,5,6], target = 5
+ * output: 2
+ * hint: binary search
+ */
+
+class Solution {
+public:
+    int searchInsert(vector<int> &nums, int target) {
+        int l = 0;
+        int r = nums.size();
+        
+        while (l < r) {
+            int m = l + (r - l) / 2;
+            
+            if (target == nums[m])
+                return m;
+            else if (target < nums[m])
+                r = m;
+            else
+                l = m + 1;
+        }
+        
+        return l;
+    }
+};
