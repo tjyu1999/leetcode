16a756a1137c48bb43ab6eae5345464cd3721f7e
tjyu1999 <tjyu1999@gmail.com>
2024-10-22 23:11:00 +0800

    edit 0035.cpp

--- 0035.cpp
+++ 0035.cpp
@@ -1,8 +1,8 @@
 /* search insert position
  *
- * input: nums = [1,3,5,6], target = 5
+ * input : nums = [1,3,5,6], target = 5
  * output: 2
- * hint: binary search
+ * hint  : binary search
  */
 
 class Solution {
