f1b6201b4bd88a3533d99d11ecc940b7bccf53b4
tjyu1999 <tjyu1999@gmail.com>
2024-10-25 00:10:30 +0800

    edit 0021.cpp

--- 0021.cpp
+++ 0021.cpp
@@ -1,8 +1,8 @@
 /* merged two sorted list
  * 
- * input: list1 = [1,2,4], list2 = [1,3,4]
- * output: [1,1,2,3,4,4]
- * hint: recursion
+ * input : list1 = [1, 2, 4], list2 = [1, 3, 4]
+ * output: [1, 1, 2, 3, 4, 4]
+ * hint  : recursion
  */
 
 class Solution {
