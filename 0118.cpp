f9f5d3025550291bc9cdbd62f5f80d90ccbc3408
tjyu1999 <tjyu1999@gmail.com>
2024-10-27 00:02:44 +0800

    edit 0118.cpp

--- 0118.cpp
+++ 0118.cpp
@@ -1,8 +1,8 @@
 /* pascal's triangle
  * 
- * input: numRows = 5
+ * input : numRows = 5
  * output: [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]]
- * hint: dynamic programming
+ * hint  : dynamic programming
  */
 
 class Solution {
