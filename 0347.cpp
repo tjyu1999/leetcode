50cea123a70e53446ede28251974bdcc9d023174
tjyu1999 <tjyu1999@gmail.com>
2024-10-30 22:52:28 +0800

    add 0347.cpp

---
+++ 0347.cpp
@@ -0,0 +1,26 @@
+/* top k elements
+ *
+ * input : nums = [1, 1, 1, 2, 2, 3], k = 2
+ * output: [1, 2]
+ */
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int> &nums, int k) {
+        vector<int> ans;
+        unordered_map<int, int> map;
+        priority_queue<pair<int, int>> pq;
+        
+        for (auto n : nums)
+            ++map[n];
+        for (auto it : map)
+            pq.push({it.second, it.first});
+        
+        while (k--) {
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        
+        return ans;
+    }
+};
