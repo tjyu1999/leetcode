49954a8fc35ba474005922597e41622bbf74e975
tjyu1999 <tjyu1999@gmail.com>
2024-12-15 23:31:57 +0800

    add 0169.cpp

---
+++ 0169.cpp
@@ -0,0 +1,22 @@
+/* majority element
+ *
+ * input : nums = [3, 2, 3]
+ * output: 3
+ */
+
+class Solution {
+public:
+    int majorityElement(vector<int> &nums) {
+        int ans;
+        int count = 0;
+
+        for (auto n: nums) {
+            if (count == 0)
+                ans = n;
+            
+            count += (n == ans) ? 1 : -1;
+        }
+
+        return ans;
+    }
+};
