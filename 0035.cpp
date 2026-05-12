961369a461f6cdd6e73a350bdc343c835dec0a4a
tjyu1999 <tjyu1999@gmail.com>
2026-05-12 22:25:20 +0800

    leetcode(35): solve search insert position

---
+++ 0035.cpp
@@ -0,0 +1,27 @@
+/* 35. Search Insert Position
+ *
+ * Time:  O(log(n))
+ * Space: O(1)
+ */
+
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int l = 0;
+        int r = nums.size();
+
+        while (l < r) {
+            const int m = (l + r) / 2;
+
+            if (nums[m] == target)
+                return m;
+            if (nums[m] < target)
+                l = m + 1;
+            else
+                r = m;
+        }
+
+        return l;
+    }
+};
