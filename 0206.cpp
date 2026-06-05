effd8d0d0bb28eb8be1f7d2089f9645048fc9596
tjyu1999 <tjyu1999@gmail.com>
2026-06-05 23:40:39 +0800

    leetcode(206): solve reverse linked list

---
+++ 0206.cpp
@@ -0,0 +1,32 @@
+/* 205. Reverse Linked List
+ *
+ * Time:  O(n)
+ * Space: O(1)
+ */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode *prev = nullptr;
+
+        while (head) {
+            ListNode *next = head->next;
+            head->next = prev;
+            prev = head;
+            head = next;
+        }
+
+        return prev;
+    }
+};
